6fc7be208eb6084b2ee2a432881ac448bcad6477
fadil-27 <fadilahmuhammad237@gmail.com>
2024-06-12 04:36:41 +0800

    Membuat Konstruktor dan setPenjumlahan

--- src/main/java/hitung/matematika.java
+++ src/main/java/hitung/matematika.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author LENOVO
- */
 public class matematika {
-    
+    //membuat variabel 
+   private double bil1, bil2;
+   
+   //membuat konstruktor
+   public matematika(double bil1, double bil2){
+      this.bil1 = bil1;
+      this.bil2 = bil2;
+   }
+   
+   //membuat method setPenjumlahan
+   public double setPenjumlahan(){
+       return bil1 + bil2;
+   }
 }
