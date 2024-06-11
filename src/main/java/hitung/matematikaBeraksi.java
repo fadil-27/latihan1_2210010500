ad5e7d06ef2716b7168e5fec49c6508e94216180
fadil-27 <fadilahmuhammad237@gmail.com>
2024-06-12 04:48:14 +0800

    Membuat kelas matematika beraksi

--- src/main/java/hitung/matematikaBeraksi.java
+++ src/main/java/hitung/matematikaBeraksi.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author LENOVO
- */
 public class matematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat obyek
+        matematika fadil = new matematika(0,0);
+        
+        System.out.println("Hasil Penjumlahan   :  "+fadil.setPenjumlahan());
+        System.out.println("Hasil Pengurangan  :  "+fadil.setPengurangan());
+        System.out.println("Hasil Perkalian   :  "+fadil.setPerkalian());
+        System.out.println("Hasil Pembagian   :  "+fadil.setPembagian());
+    }
 }
